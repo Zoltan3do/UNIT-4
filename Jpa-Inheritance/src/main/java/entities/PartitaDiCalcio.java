62675445db2cacf4a7b932740edeb84178a1f105
Manuel Barone <baronemanuel05@gmail.com>
2024-10-10 16:40:09 +0200

    esercizio jpa inheritance

---
+++ Jpa-Inheritance/src/main/java/entities/PartitaDiCalcio.java
@@ -0,0 +1,69 @@
+package entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@DiscriminatorValue("Calcio")
+@Table(name="calcio")
+
+public class PartitaDiCalcio extends Evento {
+	private String squadraCasa, squadraOspite, squadraVincente = null;
+	private int golsSC, golsSO;
+	
+	public PartitaDiCalcio() {}
+
+	public PartitaDiCalcio(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento,
+			int maxPartecipanti, String squadraCasa, String squadraOspite, String squadraVincente, int golsSC, int golsSO) {
+		super(titolo, dataEvento, descrizione, tipoEvento, maxPartecipanti);
+		this.squadraCasa = squadraCasa;
+		this.squadraOspite = squadraOspite;
+		this.squadraVincente = squadraVincente;
+		this.golsSC = golsSC;
+		this.golsSO = golsSO;
+	}
+
+	public String getSquadraCasa() {
+		return squadraCasa;
+	}
+	public void setSquadraCasa(String squadraCasa) {
+		this.squadraCasa = squadraCasa;
+	}
+	public String getSquadraOspite() {
+		return squadraOspite;
+	}
+	public void setSquadraOspite(String squadraOspite) {
+		this.squadraOspite = squadraOspite;
+	}
+	public String getSquadraVincente() {
+		return squadraVincente;
+	}
+	public void setSquadraVincente(String squadraVincente) {
+		this.squadraVincente = squadraVincente;
+	}
+	public int getGolsSC() {
+		return golsSC;
+	}
+	public void setGolsSC(int golsSC) {
+		this.golsSC = golsSC;
+	}
+	public int getGolsSO() {
+		return golsSO;
+	}
+	public void setGolsSO(int golsSO) {
+		this.golsSO = golsSO;
+	}
+	@Override
+	public String toString() {
+		return "PartitaDiCalcio [squadraCasa=" + squadraCasa + ", squadraOspite=" + squadraOspite + ", squadraVincente="
+				+ squadraVincente + ", golsSC=" + golsSC + ", golsSO=" + golsSO + "]";
+	}
+	
+	
+	
+	
+
+}
